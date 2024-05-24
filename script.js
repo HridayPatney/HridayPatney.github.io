0872e8d4249b47f1a0c6cc1a6dd2948cf6720fb5
Hriday Patney <143528508+HridayPatney@users.noreply.github.com>
2024-05-24 19:32:34 +0530

    redisgned entire website

---
+++ script.js
@@ -0,0 +1,30 @@
+
+$("a").hover(function(){
+    $(this).css("background-color", "white");
+    $(this).css("color", "black");
+
+    }, function(){
+    $(this).css("background-color", "transparent");
+    $(this).css("color", "white");
+  });
+  $(".nav-link").hover(function(){
+    $(this).css("background-color", "transparent");
+   
+}, function(){
+    $(this).css("background-color", "transparent");
+});
+
+
+  $(".change").hover(function(){
+    $(this).css("background-color", "white");
+    $(this).css("color", "black");
+}, function(){
+    $(this).css("background-color", "transparent");
+    $(this).css("color", "white");
+});
+  $(".navbar-brand").hover(function(){
+    $(this).css("background-color", "transparent");
+    }, function(){
+    $(this).css("background-color", "transparent");
+});
+
